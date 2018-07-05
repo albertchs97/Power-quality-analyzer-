166169174532b4ef6d87f343d773b26eeb3c067a
Albert Chong <albertchs97@outlook.com>
2018-07-05 10:59:15 +0800

    Update main.c

--- Summer_year3_2018/Analog_to_DIgit/main.c
+++ Summer_year3_2018/Analog_to_DIgit/main.c
@@ -1,54 +1,91 @@
+
 #include "io430.h"
 
-const unsigned char LEDtab[16] ={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};
-const unsigned char seg[4] ={BIT3,BIT4,BIT5,BIT6};
-unsigned int dig[4];
-unsigned int num,buffer;
 
-
+char buffer[1000]; // buffer for AD
+int temp;
+unsigned int j = 0; // buffer[CHANNEL-j]
+unsigned int k = 0; // buffer[k], k<2
+unsigned int ready =0; // ready for transfer AD to pc from MSP430
 
 void ADC_init()
 {
-  P6SEL |= BIT0; //P1.0 模拟信号输入引脚
-  //ADC10CTL1 |= INCH_0 + 
-  /*RESET : 
-    // ADC10CTL1
-      P1.0 作为模拟信号输入引脚；
-      通道选择 INCH_0 ;
-      工作时钟 ADC10OSC 约5MHz ; ==> ADC10CLK = ADC10OSC;
-      采样触发源：ADC10SC （软件控制）；
-      转换方式：单通道单次 ；
-      时钟源分频：/1;
-    //ADC10CTL0
-      参考电压：VR+ = AVcc , VR- = AVSS ;
-      采样时间：4*ADC10CLK;
-      最大采样率： 200ksps ;
-      中断允许控制： 禁止中断
-  */
+    P6SEL |= BIT0 ;     //P6.0 模拟输入
+    
+    ADC12CTL1 |= SHS0 ; //timer 触发
+    ADC12IE |= BIT0;    //P6.0 使能
+    
   ADC12CTL0 |= ADC12ON + ENC; //给内核供电，允许转换
-//  ADC10CTL0 |= ADC10IE;
+ //ADC12CTL0 |= ADC12IE; //允许ADCMEM中断
 }
 
-void delay()
-{
-  int j;
-  for(j=0;j<0xff;j++);
+void Timer0_init(){
+    
+    //P1.6 output of timer in compare mode 
+    P1SEL |= BIT2;
+    P1DIR |= BIT2;
+    TACCR0 = 15;  //PERIOD= 1/50000 < (TACCR0 +1 )*(1/32767) = 1/50000
+    TACCR1 = 11;   //dUTY RATIO = 25% => TA1CCR1 = 7 * (1-0.25)
+    TACCTL1 |= OUTMOD_2;   // out1_mode => toggle / reset
+    
+    TACTL = TACLR + TASSEL0 + MC0 ;  //clr TAR , ACLK be the clock of timer , up_mode
+    TACTL &= ~TAIFG;
+    //TACCTL1 |= CCIE;   //允许定时器CCR1中断 
+  
 }
+
+void USCIA0_init(){
+    //P3.6 => TXD1 funtion
+    //P3.7 => RXD1 funtion
+    // P3OUT &= ~(BIT6 + BIT7);
+    P3DIR   |= BIT6;
+    P3SEL   |= BIT6  ;   //activate USC1_A1 TXC/RXD
+    ME2     |= UTXE1 ;   //Module Enable RXD1 & TXD1
+    U1CTL   |= CHAR;            //chosse for 8-digit character length
+    U1TCTL  |= SSEL0;           //CHOOSE ACLK AS CLK SOURCE CLK
+    U1BR0   =  0x03;            //BODE RATE AS 9600
+    U1BR1   =  0x00;            //
+    U1MCTL  =  0x4A;            //BODE RATE MODULATION
+    U1CTL   &= ~SWRST;          //RESET RELEASED FOR OPERATION
+ //   IE2 |= UTXIE1;    
+}
+
 int main( void )
 {
   // Stop watchdog timer to prevent time out reset
   WDTCTL = WDTPW + WDTHOLD;
-  int i ; 
-  ADC_init();
- // __enable_interrupt();
-//  ADC10CTL0 |= ADC10SC  ;
-  
-    
-  while(1)
-  {
-   ADC12CTL0 |= ADC12SC ; //软件启动采样
-   while((BIT0 & ADC12IFG ) == 0 ) ;
-    buffer = ADC12MEM0;
+  Timer0_init();        //定时器初始化
+  ADC_init();           //AD转换初始化
+  USCIA0_init();        //串口通信初始化
+  __enable_interrupt(); //开总控位
+  P2DIR |= BIT7;
+  while(1){
+    if (j == 1000)
+    {
+      ADC12IE &= ~BIT0;
+      for (int i = 0; i != 1000; ++i)
+      {
+        while (IFG2 & BIT5 == 0) ;
+          U1TXBUF = buffer[i];
+      }
+      j = 0;
+      ADC12IE |= BIT0;
+    }
   }
   return 0;
+}
+
+
+// ADMEM Interrupt
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC10_isr(){
+  if (j != 1000)
+  {
+    temp = ADC12MEM0;
+    buffer[j]= (char)temp;
+    buffer[j + 1] = (char)(temp >> 8);
+    j += 2;
+    ADC12CTL0 &= ~EN
+    ADC12CTL0 |= ENC;
+  }
 }
